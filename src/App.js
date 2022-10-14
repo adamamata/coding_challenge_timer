ef9b5ed707e602e326baf7b5b7ce8c290146b519
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-14 17:23:20 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
 
   const [count, setCount] = useState(10);
   const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    let interval = null;
-    if (isActive) {
-      interval = setInterval(() => {
-          setCount((count) => count - 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    } 
-  }, [isActive])
-
   const handleClick = (e) => {
-    e.preventDefault();
     setIsActive(true);
   }
 
+  if (isActive === true) {
+    let interval = setInterval(() => {
+      if (count > 0){
+        setCount(count - 1);
+      }
+    }, 1000);
+    if (count === 0) {
+      setIsActive(false);
+      clearInterval(interval);
+    }
+  }
+
+
   return (
     <div className="App">
-
-        <label>Time:</label>
-        {/* <input onChange={handleChange} name="input"/> */}
-        <button onClick={handleClick}>Count Down</button>
-        
-      <div className="count">
-        <h1>{count}</h1>
-      </div>
+      <button onClick={handleClick}>Time</button>
+      <h1>{count}</h1>
+      {count === 0 && 
+        <h1>Done!</h1>
+      }
     </div>
   );
 }
