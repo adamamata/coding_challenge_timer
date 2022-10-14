3f88bdacf5e204c32901c89a056689b4949fd2eb
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-14 06:20:16 +0200

    bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(100);
+
+  function countDown(){
+    const current = count;
+    for (let i = 0; i < current; i++){
+      setTimeout(() => {
+        setCount(count - 1);
+      }, 1000)
+    }
+  }
+
+  // const handleChange = (e) => {
+  //   e.preventDefualt();
+  // }
+
+  const handleSubmit = () => {
+    countDown();
+  }
+
+
   return (
     <div className="App">
 
+        <label>Time:</label>
+        {/* <input onChange={handleChange} name="input"/> */}
+        <button onClick={handleSubmit}>Submit</button>   
+
+      <div className="count">
+        <h1>{count}</h1>
+      </div>
     </div>
   );
 }
